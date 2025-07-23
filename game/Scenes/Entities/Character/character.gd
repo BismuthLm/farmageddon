89c499a90e283bed2a112073b9b9f5c4142eba4a
charlieWHS <wilkich@whs.school.nz>
2025-07-23 20:04:33 +1200

    Made the animations work while walking

--- game/Scenes/Entities/Character/character.gd
+++ game/Scenes/Entities/Character/character.gd
@@ -1,13 +1,41 @@
 extends CharacterBody2D
 
 
-@export var max_speed: int = 500
+@export var max_speed: int = 300
 var speed: int = max_speed
 
 func _process(_delta):
 	
-	# input 
+	#Input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
 	Globals.player_pos = global_position
+	
+	
+	if Input.is_action_pressed("left"):
+		$AnimationPlayer.play("Walking Left")
+		
+	if Input.is_action_just_released("left"):
+		$AnimationPlayer.pause()
+		
+		
+	if Input.is_action_pressed("right"):
+		$AnimationPlayer.play("Walking Right")
+		
+	if Input.is_action_just_released("right"):
+		$AnimationPlayer.pause()
+		
+		
+	if Input.is_action_pressed("up"):
+		$AnimationPlayer.play("Walking Backwards")
+		
+	if Input.is_action_just_released("up"):
+		$AnimationPlayer.pause()
+		
+		
+	if Input.is_action_pressed("down"):
+		$AnimationPlayer.play("Walking Forward")
+		
+	if Input.is_action_just_released("down"):
+		$AnimationPlayer.pause()
