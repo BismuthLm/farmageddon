3d4568b8722cebee62005855a2d748cadc7ab834
CharlieWHS <wilkich@whs.school.nz>
2025-08-04 10:35:11 +1200

    Farming mechanic

--- game/Scenes/Main/main_scene.gd
+++ game/Scenes/Main/main_scene.gd
@@ -2,6 +2,24 @@ extends Node2D
 
 signal plant_sleep
 
+var plant_scene: PackedScene = preload("res://Scenes/Objects/Plant/plant.tscn")
+
+func _ready():
+
+		
+		var pos = $"Plant Markers/PlantMarker".position
+		var plant = plant_scene.instantiate() as Node2D
+		plant.global_position = pos
+		$Plants.add_child(plant)
+		
+		pos = $"Plant Markers/PlantMarker2".position
+		plant = plant_scene.instantiate() as Node2D
+		plant.global_position = pos
+		$Plants.add_child(plant)
+
+
+
+
 func _on_hour_timer_timeout():
 	if Globals.time_of_day <= 23:
 		Globals.time_of_day += 1
