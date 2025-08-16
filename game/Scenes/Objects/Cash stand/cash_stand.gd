d1026bc54ba79a91dc4d2cb47bc9be953101361e
charlieWHS <wilkich@whs.school.nz>
2025-08-16 20:39:33 +1200

    started code for selling and buying stuff

---
+++ game/Scenes/Objects/Cash stand/cash_stand.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+var cash_stand: bool = false
+
+
+
+func _on_area_2d_body_entered(body):
+	if body in get_tree().get_nodes_in_group('Character'):
+		cash_stand = true
+		
+
+
+
+func _on_area_2d_body_exited(body):
+	pass # Replace with function body.
