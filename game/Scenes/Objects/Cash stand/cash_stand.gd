7940b8c8a29c8e148dbccfeb9cbfc7a78c088153
charlieWHS <wilkich@whs.school.nz>
2025-08-16 20:40:20 +1200

    more stuff

--- game/Scenes/Objects/Cash stand/cash_stand.gd
+++ game/Scenes/Objects/Cash stand/cash_stand.gd
@@ -12,4 +12,5 @@ func _on_area_2d_body_entered(body):
 
 
 func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
+	if body in get_tree().get_nodes_in_group('Character'):
+		cash_stand = false
