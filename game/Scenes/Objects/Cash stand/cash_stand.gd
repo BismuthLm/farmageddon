d7252c8a3c3e3330aa42db1724556135110efb15
CharlieWHS <wilkich@whs.school.nz>
2025-08-18 10:38:35 +1200

    stuff

--- game/Scenes/Objects/Cash stand/cash_stand.gd
+++ game/Scenes/Objects/Cash stand/cash_stand.gd
@@ -5,12 +5,17 @@ extends StaticBody2D
 
 
 func _on_area_2d_body_entered(body):
+	print(body)
 	if body in get_tree().get_nodes_in_group('Character'):
 		Globals.cash_stand = true
-		
 
 
 
 func _on_area_2d_body_exited(body):
 	if body in get_tree().get_nodes_in_group('Character'):
 		Globals.cash_stand = false
+
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body in get_tree().get_nodes_in_group('Character'):
+		Globals.cash_stand = true
