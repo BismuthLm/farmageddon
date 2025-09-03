c8bdd39d421b5e67bc111c3699a8065a7daf68cf
CharlieWHS <wilkich@whs.school.nz>
2025-09-04 11:59:12 +1200

    fixed whole house

--- game/Scenes/Main/Level/level.gd
+++ game/Scenes/Main/Level/level.gd
@@ -5,12 +5,12 @@ extends Node2D
 func _on_door_area_entry_area_entered(area):
 	if area in get_tree().get_nodes_in_group("Character"):
 		$"HouseTop".visible = false
-		$"HouseBottom".visible = false
+		$YSort/HouseBottom.visible = false
 		$YSort/Inside.visible = true
 
 #makes the outside visible
 func _on_door_area_exit_area_entered(area):
 	if area in get_tree().get_nodes_in_group("Character"):
 		$"HouseTop".visible = true
-		$"HouseBottom".visible = true
+		$YSort/HouseBottom.visible = true
 		$YSort/Inside.visible = false
